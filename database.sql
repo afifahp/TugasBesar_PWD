a9fde601fa882d05698fdb335c2e7be9b34a58bf
afifahp <fifahputriw@gmail.com>
2022-12-12 11:35:17 +0700

    update ygy

--- database.sql
+++ database.sql
@@ -1,8 +1,8 @@
-create database EmoteHelp
+CREATE DATABASE EmoteHelp
 
-use EmoteHelp
+USE EmoteHelp
 
-create table Pegawai(
+CREATE TABLE Pegawai(
     nip int (10) PRIMARY KEY not null,
     nama_pegawai char (50) not null,
     j_kel varchar(50),
@@ -11,10 +11,10 @@ create table Pegawai(
     e_mail varchar(50) not null,
     no_hp varchar(16) not null,
     jdw_kerja char(16) not null,
-        type ENUM ('Pagi', 'Siang', 'Sore') not null,
-);
+        SCHEDULE ENUM ('Pagi', 'Siang', 'Sore') not null
+)
 
-create table Pelanggan(
+CREATE TABLE Pelanggan(
     kode_book varchar(10) PRIMARY KEY not null,
     tgl_book datetime not null,
     tgl_bayar datetime not null,
@@ -23,16 +23,19 @@ create table Pelanggan(
         type ENUM ('Laki-laki', 'Perempuan') not null,
     alamat varchar(50) not null,
     no_hp varchar(16) not null,
-    jenis_ply varchar(20) constraint cektype check (jenis_ply in ('Coret', 'Mewarnai', 'Hancurkan')),
-        FOREIGN KEY REFERENCES Pelayanan(jenis_ply) not null
-;)
+    jenis_ply varchar(20) not null, 
+    	FOREIGN KEY fk_Pel(jenis_ply)
+    	REFERENCES Pelayanan(jenis_ply)
+)
 
-create table Pelayanan(
+CREATE TABLE Pelayanan(
     jenis_ply varchar(20) PRIMARY KEY not null,
-    harga decimal not null,
-);
+    harga decimal (10,2) not null
+)
 
-create table Transaksi(
+CREATE TABLE Transaksi(
     no_urut int(5) PRIMARY KEY not null,
-    kode_book varchar(10) FOREIGN KEY REFERENCES Pelanggan(kode_book) not null
+    kode_book varchar(10) not null,
+    	FOREIGN KEY fk_Pelanggan(kode_book)
+    	REFERENCES Pelanggan(kode_book)
 )
