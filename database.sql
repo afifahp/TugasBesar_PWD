a152d4450f0685cada519c01c63e6a7ae7165f51
Ramadika Dimas N <120315341+RamadikaDN@users.noreply.github.com>
2023-01-03 21:13:30 +0700

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,38 +1,12 @@
-create database EmoteHelp
-
-use EmoteHelp
-
-create table Pegawai(
-    nip int (10) PRIMARY KEY not null,
-    nama_pegawai char (50) not null,
-    j_kel varchar(50),
-        type ENUM ('Laki-laki', 'Perempuan') not null,
-    alamat varchar(50) not null,
-    e_mail varchar(50) not null,
-    no_hp varchar(16) not null,
-    jdw_kerja char(16) not null,
-        type ENUM ('Pagi', 'Siang', 'Sore') not null,
-);
-
-create table Pelanggan(
-    kode_book varchar(10) PRIMARY KEY not null,
-    tgl_book datetime not null,
-    tgl_bayar datetime not null,
-    nama_plg char(50) not null,
-    j_kel varchar(50),
-        type ENUM ('Laki-laki', 'Perempuan') not null,
-    alamat varchar(50) not null,
-    no_hp varchar(16) not null,
-    jenis_ply varchar(20) constraint cektype check (jenis_ply in ('Coret', 'Mewarnai', 'Hancurkan')),
-        FOREIGN KEY REFERENCES Pelayanan(jenis_ply) not null
-;)
-
-create table Pelayanan(
-    jenis_ply varchar(20) PRIMARY KEY not null,
-    harga decimal not null,
-);
-
-create table Transaksi(
-    no_urut int(5) PRIMARY KEY not null,
-    kode_book varchar(10) FOREIGN KEY REFERENCES Pelanggan(kode_book) not null
-)
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+CREATE TABLE `login` (
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+INSERT INTO `login` (`username`, `password`) VALUES
+('admin', '21232f297a57a5a743894a0e4a801fc3');
+ALTER TABLE `login`
+  ADD UNIQUE KEY `username` (`username`);
+COMMIT;
